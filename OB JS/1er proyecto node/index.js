2980315e57d1d9860bb58302baa5fdebd40d30d2
ImomushiRan <george.villarroel@outlook.es>
2022-09-02 04:42:53 -0300

    ej1

---
+++ OB JS/1er proyecto node/index.js
@@ -0,0 +1,12 @@
+/* This is the welcome message */
+console.log("This is the main entrance to the project")
+
+/*  This is the middle part of our project
+ In this part we do the last checks neccesary
+ and at last we go to the goodbye message
+ This is commented
+ this too
+ */
+
+/* This is a goodbye message */
+console.log("Goodbye")
